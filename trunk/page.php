fcf10ec891850931581e6bcf208003c23659e76f
afonsogfrgarcia@gmail.com <afonsogfrgarcia@gmail.com@bab1db3c-fa9c-fd79-9bd5-b06ae451f869>
2012-02-19 10:31:12 +0000

    Updated HTMLVisitor and Page visitor to generate the necessary HTML headers for RSS autodiscovery. Added a variable to globalvars.php for the folder where ABBOV is running.

--- trunk/page.php
+++ trunk/page.php
@@ -4,7 +4,7 @@
 	include "settings/globalvars.php";
 
 	$blog = new Blog($GLOB_username, $GLOB_password, $GLOB_server, $GLOB_database);
-	$visitor = new PageVisitor($_GET['page']);
+	$visitor = new PageVisitor($_GET['page'], $GLOB_folder);
 
 	$blog->accept($visitor);
 ?>
